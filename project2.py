509ee15838c9ce07902091c5bdc9319ef1261421
Lewis-Ho <yiucheungho@Yius-MacBook-Pro.local>
2015-04-18 11:50:28 -0400

    py2neo setup

---
+++ project2.py
@@ -0,0 +1,15 @@
+Python 3.4.2 (v3.4.2:ab2c023a9432, Oct  5 2014, 20:42:22) 
+[GCC 4.2.1 (Apple Inc. build 5666) (dot 3)] on darwin
+Type "copyright", "credits" or "license()" for more information.
+>>> WARNING: The version of Tcl/Tk (8.5.9) in use may be unstable.
+Visit http://www.python.org/download/mac/tcltk/ for current information.
+from py2neo import Graph, Path
+graph = Graph()
+
+tx = graph.cypher.begin()
+for name in ["Alice", "Bob", "Carol"]:
+    tx.append("CREATE (person:Person {name:{name}}) RETURN person", name=name)
+alice, bob, carol = [result.one for result in tx.commit()]
+
+friends = Path(alice, "KNOWS", bob, "KNOWS", carol)
+graph.create(friends)
